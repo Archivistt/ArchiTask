071572ccc79384ed25c8244fca33eac02c58aa3a
philophobiccharacter <philophobiccharacter@gmail.com>
2024-12-15 12:33:32 +0000

    initialized shadcn and chakraUI

---
+++ src/EndDrawer.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { Input, Stack } from "@chakra-ui/react"
+import { Button } from "@/components/ui/button"
+import {
+  DrawerActionTrigger,
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { useRef } from "react"
+
+const EndDrawer = () => {
+  const ref = useRef<HTMLInputElement>(null)
+  return (
+    <DrawerRoot initialFocusEl={() => ref.current}>
+      <DrawerBackdrop />
+      <DrawerTrigger asChild>
+        <Button variant="outline" size="sm">
+          Open Drawer
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Drawer Title</DrawerTitle>
+        </DrawerHeader>
+        <DrawerBody>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Stack mt="5">
+            <Input defaultValue="Naruto" placeholder="First name" />
+            <Input ref={ref} placeholder="Email" />
+          </Stack>
+        </DrawerBody>
+        <DrawerFooter>
+          <DrawerActionTrigger asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerActionTrigger>
+          <Button>Save</Button>
+        </DrawerFooter>
+        <DrawerCloseTrigger />
+      </DrawerContent>
+    </DrawerRoot>
+  )
+}
+
+export default EndDrawer
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import EndDrawer from "./EndDrawer.jsx"
+
 
 
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />,    
-    </StrictMode>
-    
+    <Provider>
+        <StrictMode>
+            <EndDrawer />,    
+        </StrictMode>
+    </Provider>
 )
