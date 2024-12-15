f0a82e90397b8f23ddaebed99c3718a65506f09f
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-15 12:09:56 +0000

    Base Files

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import DarkMode from './components/DarkMode.jsx'
-import Drop from './components/Drop.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
-    <App />,
+    <StrictMode>
+        <App />,    
+    </StrictMode>
+    
 )
